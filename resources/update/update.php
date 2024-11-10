1414740d6700e2fecd238678543d451fa989a1e0
fewfre <wesitup@gmail.com>
2024-11-10 00:33:58 -0500

    badges now update async

--- resources/update/update.php
+++ resources/update/update.php
@@ -1,143 +1,180 @@
 <?php
 require_once 'utils.php';
-
-ini_set('display_errors', '1');
-ini_set('display_startup_errors', '1');
-error_reporting(E_ALL);
-
-ini_set('max_execution_time', 5*60);
-set_time_limit(5*60);
-
-$resources = array();
-$external = array();
+define('URL_TO_CHECK_IF_SCRIPT_HAS_ACCESS_TO_ASSETS', "http://www.transformice.com/images/x_bibliotheques/x_macarons.swf");
 
 setProgress('starting');
 
-// file_put_contents("testing.json", json_encode(array()));
-// function ADD_LOG($msg) {
-// 	$json = json_decode(file_get_contents("testing.json"), true);
-// 	$json[] = $msg;
-// 	file_put_contents("testing.json", json_encode($json, JSON_PRETTY_PRINT));
-// }
-// // Ping to confirm if server is booting us
-// function ping($host, $port, $timeout) { $tB = microtime(true); $fP = fSockOpen($host, $port, $errno, $errstr, $timeout); if (!$fP) { return "down"; } $tA = microtime(true); return round((($tA - $tB) * 1000), 0)." ms"; }
-// ADD_LOG( ping('www.transformice.com', 80, 100) );
-
 // Check if Atelier801 server can be accessed
-$isA801ServerOnline = fetchUrlMetaData("http://www.transformice.com/images/x_bibliotheques/x_macarons.swf");
+$isA801ServerOnline = fetchUrlMetaData(URL_TO_CHECK_IF_SCRIPT_HAS_ACCESS_TO_ASSETS);
 if(!$isA801ServerOnline['exists']) {
 	setProgress('error', [ 'message' => "Update script cannot currently access the Atelier 801 servers - it may either be down, or script might be blocked/timed out" ]);
 	exit;
 }
 
-//
-// Multi-item pack Loading
-//
-$resources_base = array("x_items_chaman", "x_macarons");
-foreach ($resources_base as $filebase) {
-	setProgress('updating', [ 'message'=>"Resource: $filebase", 'value'=>1, 'max'=>1 ]);
-	$filename = "{$filebase}.swf";
-	$url = "http://www.transformice.com/images/x_bibliotheques/$filename";
-	$file = "../$filename";
-	downloadFileIfNewer($url, $file);
-	
-	// Check local file so that if there's a load issue the update script still uses the current saved version
-	if(file_exists($file)) {
-		$resources[] = $filename;
-		$external[] = $url;
-	}
-}
+updateBasicResources();
+function updateBasicResources() {
+	$resources = array();
+	$external = array();
 
-//
-// Single item swf Loading
-//
-
-// type/start number (since only new ones use this system)
-$types = array(
-	[1, 43, "box (small)"],
-	[2, 47, "box (large)"],
-	[3, 41, "plank (small)"],
-	[4, 44, "plank (large)"],
-	[6, 37, "ball"],
-	[7, 10, "trampoline"],
-	[10, 22, "anvil"],
-	[17, 36, "cannonball"],
-	[28, 45, "balloon"]
-);
-foreach ($types as $typedata) {
-	list($type, $start, $typeName) = $typedata;
-	$max = ($start+100);
-	$breakCount = 0; // quit early if enough 404s in a row
-	for ($i = $start; $i <= $max; $i++) {
-		setProgress('updating', [ 'message'=>"Item Type: $type [$typeName]", 'value'=>$i-$start+1, 'max'=>$max-$start ]);
-		
-		$filename = "o{$type},$i.swf";
-		$url = "http://www.transformice.com/images/x_bibliotheques/chamanes/$filename";
-		$filenameLocal = "items/$filename";
-		$file = "../$filenameLocal";
+	//
+	// Multi-item pack Loading
+	//
+	$resources_base = array("x_items_chaman", "x_macarons");
+	foreach ($resources_base as $filebase) {
+		setProgress('updating', [ 'message'=>"Resource: $filebase", 'value'=>1, 'max'=>1 ]);
+		$filename = "{$filebase}.swf";
+		$url = "http://www.transformice.com/images/x_bibliotheques/$filename";
+		$file = "../$filename";
 		downloadFileIfNewer($url, $file);
-	
+		
 		// Check local file so that if there's a load issue the update script still uses the current saved version
 		if(file_exists($file)) {
-			$resources[] = $filenameLocal;
+			$resources[] = $filename;
 			$external[] = $url;
+		}
+	}
+
+	//
+	// Single item swf Loading
+	//
+
+	// type/start number (since only new ones use this system)
+	$types = array(
+		[1, 43, "box (small)"],
+		[2, 47, "box (large)"],
+		[3, 41, "plank (small)"],
+		[4, 44, "plank (large)"],
+		[6, 37, "ball"],
+		[7, 10, "trampoline"],
+		[10, 22, "anvil"],
+		[17, 36, "cannonball"],
+		[28, 45, "balloon"]
+	);
+	foreach ($types as $typedata) {
+		list($type, $start, $typeName) = $typedata;
+		$max = ($start+100);
+		$breakCount = 0; // quit early if enough 404s in a row
+		for ($i = $start; $i <= $max; $i++) {
+			setProgress('updating', [ 'message'=>"Item Type: $type [$typeName]", 'value'=>$i-$start+1, 'max'=>$max-$start ]);
+			
+			$filename = "o{$type},$i.swf";
+			$url = "http://www.transformice.com/images/x_bibliotheques/chamanes/$filename";
+			$filenameLocal = "items/$filename";
+			$file = "../$filenameLocal";
+			downloadFileIfNewer($url, $file);
+		
+			// Check local file so that if there's a load issue the update script still uses the current saved version
+			if(file_exists($file)) {
+				$resources[] = $filenameLocal;
+				$external[] = $url;
+				$breakCount = 0;
+			} else {
+				$breakCount++;
+				if($breakCount > 5) { break; }
+			}
+		}
+	}
+
+	setProgress('updating');
+	
+	$json = getConfigJson();
+	$json["packs"]["items"] = $resources;
+	$json["packs_external"] = $external;
+	saveConfigJson($json);
+}
+
+updateBadges();
+function updateBadges() {
+	setProgress('updating', [ 'message'=>"Badges" ]);
+	$badgeDataList = array(); // Array<"filename.png", headers>
+	
+	$CHUNK = 50;
+	$originalStart = 352;
+	$BREAK_NUM = 5;
+	
+	function makeDataFromFileIndex($i) {
+		$filename = "x_{$i}L.png"; $filenameSmall = "x_{$i}.png";
+		return [
+			'filename' => $filename,
+			'filenameSmall' => $filenameSmall,
+			'localFilePath' => "../badges/$filename",
+			'localFilePathSmall' => "../badges/$filenameSmall",
+			'url' => "http://www.transformice.com/images/x_transformice/x_badges/$filename",
+			'urlSmall' => "http://www.transformice.com/images/x_transformice/x_badges/$filenameSmall"
+		];
+	}
+	
+	$breakCount = 0; // quit early if enough 404s in a row
+	$start = $originalStart;
+	$end = $start+$CHUNK;
+	// fetch multiple urls in large chunks then part them
+	do {
+		setProgress('updating', [ 'message'=>"Checking for badge updates: $start-$end" ]);
+		$urls = array();
+		$urlsSmall = array();
+		$chunkedDataList = array();
+		for ($i = $start; $i < $end; $i++) {
+			$data = makeDataFromFileIndex($i);
+			$chunkedDataList[] = $data;
+			$urls[] = $data['url'];
+			$urlsSmall[] = $data['urlSmall'];
+		}
+		// Fetch all the headers for the urls, then add them to the list if they exist
+		$chunkHeadersList = fetchHeadersOnlyMulti($urls);
+		$chunkHeadersListSmall = fetchHeadersOnlyMulti($urlsSmall);
+		$len = min(count($chunkedDataList), count($chunkHeadersList));
+		for ($i = 0; $i < $len; $i++) {
+			$data = $chunkedDataList[$i];
+			$data['headers'] = $chunkHeadersList[$i];
+			$data['headersSmall'] = $chunkHeadersListSmall[$i];
+			
+			if($chunkHeadersList[$i] && $chunkHeadersList[$i]['exists'] && $chunkHeadersListSmall[$i] && $chunkHeadersListSmall[$i]['exists']) {
+				$badgeDataList[] = $data;
+				$breakCount = 0;
+			} else {
+				$breakCount++;
+				if($breakCount > $BREAK_NUM) { break; }
+			}
+		}
+		// Setup values for new chunk range
+		$start = $end;
+		$end = $start+$CHUNK;
+	} while($breakCount <= $BREAK_NUM);
+	
+	// loop through returned headers and download the missing ones / ones that have been updated
+	foreach ($badgeDataList as $data) {
+		setProgress('updating', [ 'message'=>"Downloading file if needed: {$data['filename']}" ]);
+		downloadFileIfHeadersAreNewer($data['url'], $data['localFilePath'], $data['headers']);
+		downloadFileIfHeadersAreNewer($data['urlSmall'], $data['localFilePathSmall'], $data['headersSmall']);
+	}
+	
+	// Check local file before adding to list, so that if there's a load issue the update script still uses the current saved version
+	setProgress('updating', [ 'message'=>"Generating list of all badges" ]);
+	$badges = array();
+	$start = $originalStart;
+	$breakCount = 0; // quit early if enough 404s in a row
+	for ($i = $start; $i <= 1000; $i++) {
+		list('localFilePath' => $file, 'filename' => $filename) = makeDataFromFileIndex($i);
+		
+		// Check local file so that if there's a load issue the update script still uses the current saved version
+		if(file_exists($file)) {
+			$badges[] = $filename;
 			$breakCount = 0;
 		} else {
 			$breakCount++;
-			if($breakCount > 5) { break; }
-		}
-	}
-}
-
-//
-// Badge Loading
-//
-$badges = array();
-$start = 352;
-$max = ($start+1000);
-$breakCount = 0; // quit early if enough 404s in a row
-for ($i = $start; $i <= $max; $i++) {
-	setProgress('updating', [ 'message'=>"Badge: $i", 'value'=>$i-$start+1, 'max'=>$max-$start ]);
-	$filename = "x_{$i}L.png";
-	$url = "https://www.transformice.com/images/x_transformice/x_badges/$filename";
-	$file = "../badges/$filename";
-	$resp = fetchUrlMetaData($url);
-	if($resp['exists'] && checkIfUrlLastModifiedNewerThanFile($resp['lastModified'], $file)) {
-		// We only want to check if small exists after we confirm big exists (for efficiency)
-		$filenameSmall = "x_{$i}.png";
-		$urlSmall = "https://www.transformice.com/images/x_transformice/x_badges/$filenameSmall";
-		$fileSmall = "../badges/$filenameSmall";
-		$respSmall = fetchUrlMetaData($urlSmall);
-		if($resp['exists'] && $respSmall['exists']) {
-			ifUrlIsNewerThenDownloadToFile($url, $resp['lastModified'], $file);
-			ifUrlIsNewerThenDownloadToFile($urlSmall, $respSmall['lastModified'], $fileSmall);
+			if($breakCount > $BREAK_NUM) { break; }
 		}
 	}
 	
-	// Check local file so that if there's a load issue the update script still uses the current saved version
-	if(file_exists($file)) {
-		$badges[] = $filename;
-		$breakCount = 0;
-	} else {
-		$breakCount++;
-		if($breakCount > 5) { break; }
-	}
+	setProgress('updating');
+	
+	$json = getConfigJson();
+	$json["badges"] = $badges;
+	saveConfigJson($json);
 }
-
-setProgress('updating');
-
-$json_path = "../config.json";
-$json = json_decode(file_get_contents($json_path), true);
-$json["packs"]["items"] = $resources;
-$json["packs_external"] = $external;
-$json["badges"] = $badges;
-$json["cachebreaker"] = time();//md5(time(), true);
-file_put_contents($json_path, json_encode($json));//, JSON_PRETTY_PRINT
 
 setProgress('completed');
 echo "Update Successful!";
 
 sleep(10);
 setProgress('idle');
-// echo "Update Successful! Redirecting...";
-// echo '<script>window.setTimeout(function(){ window.location = "../"; },1000);</script>';
